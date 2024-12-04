9c0f17e67428c463457d3bcff76fb604153da05a
Thejan-Rodrigo <thejanpandula2003@gmail.com>
2024-12-05 02:40:23 +0530

    get configData method
    get data from the user for the Configure parameters

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 import com.google.gson.Gson;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration(10, 55, 70, 50);
+        int totTickets = totalTickets();
+        int ticketRelRate = ticketReleaseRate();
+        int cusRetRate = customerRetrievalRate();
+        int maxTicketCap = maxTicketCapacity();
+
+        System.out.println(totTickets);
+        System.out.println(ticketRelRate);
+        System.out.println(cusRetRate);
+        System.out.println(maxTicketCap);
+
+        Configuration configuration = new Configuration(totTickets, ticketRelRate, cusRetRate, maxTicketCap);
 
         configuration.saveConfig(configuration);
         configuration.loadConfig();
@@ -17,4 +29,31 @@ public class Main {
         Thread customerThread = new Thread(customer1, "customer");
         customerThread.start();
     }
+
+    public static int totalTickets(){
+        Scanner input1 = new Scanner(System.in);
+        System.out.print("Enter Total Number of Tickets: ");
+        return input1.nextInt();
+    }
+
+
+    public static int ticketReleaseRate(){
+        Scanner input2 = new Scanner(System.in);
+        System.out.print("Enter Ticket Release Rate: ");
+        return input2.nextInt();
+    }
+
+    public static int customerRetrievalRate(){
+        Scanner input3 = new Scanner(System.in);
+        System.out.print("Enter Customer Retrieval Rate: ");
+        return input3.nextInt();
+
+    }
+
+    public static int maxTicketCapacity(){
+        Scanner input4 = new Scanner(System.in);
+        System.out.print("Enter Maximum Ticket Capacity: ");
+        return input4.nextInt();
+    }
+
 }
