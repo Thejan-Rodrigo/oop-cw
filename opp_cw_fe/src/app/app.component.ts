21337ff30e77fc701c1c7039f42dd56c5535d86c
Thejan-Rodrigo <thejanpandula2003@gmail.com>
2024-12-05 22:45:00 +0530

    Create components
    create component
    Create config-form
    Create nav bar

--- opp_cw_fe/src/app/app.component.ts
+++ opp_cw_fe/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ConfigFormComponent } from './components/config-form/config-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ConfigFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
